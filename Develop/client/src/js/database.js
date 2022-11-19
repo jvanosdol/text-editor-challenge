ee86f78b4ea8cb11cf6820f7fe295ddf56adb175
jvanosdol <j.vanosdol99@yahoo.com>
2022-11-18 21:28:34 -0800

    installed babel, also completed database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,55 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+
+// **********
+// found code in week 19, day 3, activity 23, brought in the get and put methods and tweaked code to better fit the project's context
+// **********
+
+export const putDb = async (content) => {
+  const textDb = await openDB('text', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = textDb.transaction('text', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('text');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.add({ text: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+}
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // Create a connection to the database database and version we want to use.
+  const textDb = await openDB('text', 1);
+
+   // Create a new transaction and specify the database and data privileges.
+  const tx = textDb.transaction('text', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('text');
+
+   // Use the .get() method to get a piece of data from the database based on the id.
+  const request = store.get(id);
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
+
+  
+
+
+
+
 
 initdb();
