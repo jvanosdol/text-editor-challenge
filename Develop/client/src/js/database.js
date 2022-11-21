078c952e32c6c2669e097aa2d2ee118dc335e887
jvanosdol <j.vanosdol99@yahoo.com>
2022-11-20 17:10:49 -0800

    added to install.js and changed paths in webpack.config.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -29,7 +29,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ id: 1, text: content });
+  const request = store.add({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -54,8 +54,8 @@ export const getDb = async () => {
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result.value);
+  return result.value;
 }
 
   
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -43,7 +43,7 @@ module.exports = () => {
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swDest: 'service-worker.js',
       }),
     ],
 
